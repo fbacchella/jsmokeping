183c674ee7fae700560a6a14b06117fd619d0a27
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2023-08-22 18:46:10 +0200

    Improved graph rendering

--- src/main/java/jrds/smokeping/SmokeGraph.java
+++ src/main/java/jrds/smokeping/SmokeGraph.java
@@ -27,30 +27,29 @@ public class SmokeGraph extends jrds.Graph {
         super.setGraphDefData(graphDef, defProbe, ei, customData);
         graphDef.setRigid(true);
         graphDef.setMinValue(0);
-        graphDef.setMaxValue(getMax(defProbe, ei));
-        graphDef.datasource("stddev", "median", new Variable.STDDEV());
-        graphDef.datasource("max" + PERCENTILE, "max", new Variable.PERCENTILE(PERCENTILE));
+        graphDef.setMaxValue(getMax(defProbe, ei) * 1.2);
+        graphDef.setDownsampler(null);
 
-        for(int i = 1; i <= 20; i++) {
-            String ping = "ping" + i;
-            String rpn = String.format("%s,max95,LT,%s,max95,IF", ping, ping);
-            graphDef.datasource("cp" + i, rpn);
-        }
-        for(int i = 1 ; i <= 10; i++){
-            String rpn = String.format("cp%d,UN,UNKN,cp%d,cp%d,-,IF", i, 21 - i, i);
+        graphDef.datasource("stddev", "median", new Variable.STDDEV());
+
+        for (int i = 1 ; i <= 10; i++){
+            String rpn = String.format("ping%d,UN,UNKN,ping%d,ping%d,-,IF", i, 21 - i, i);
             graphDef.datasource("smoke" + i, rpn);
-            graphDef.area("cp" + i, TRANSLUCENT);
+            graphDef.area("ping" + i, TRANSLUCENT);
             graphDef.stack("smoke" + i, GRAYS[i - 1]);
         }
 
+        graphDef.comment("\\l");
+        graphDef.gprint("stddev", "Median standard deviation ± %.1lf %ss\\l");
         graphDef.comment("loss color");
         int previous = -1;
-        for(int i = 0; i <= 6; i++ ) {
+        for (int i = 0; i <= 6; i++ ) {
             int indice = i;
-            if(i == 5)
+            if (i == 5) {
                 indice = 10;
-            else if(i == 6)
+            } else if (i == 6) {
                 indice = 19;
+            }
             graphDef.datasource("me" + indice, String.format("loss,%d,GT,loss,%d,LE,*,1,UNKN,IF,median,*", previous, indice));
             graphDef.datasource("meL" + indice, String.format("me%d,stddev,-", indice));
             graphDef.datasource("meH" + indice, String.format("me%d,0,*,stddev,2,*,+", indice));
@@ -58,7 +57,7 @@ public class SmokeGraph extends jrds.Graph {
             graphDef.stack("meH" + indice, LOSS[i], indice + "/20");
             previous = indice;
         }
-        graphDef.comment("\\l");
+        graphDef.line("median", MEDIUM_GREEN);
     }
 
     private double getMax(Probe<?,?> p, ExtractInfo ei) {
@@ -75,6 +74,8 @@ public class SmokeGraph extends jrds.Graph {
     }
 
     private static final Color TRANSLUCENT = new Color(255, 255, 255, 0);
+    private static final Color MEDIUM_GREEN = new Color(0x029602);
+
     private static final Color[] GRAYS = Stream.of(0xdddddd, 0xcacaca, 0xb7b7b7, 0xa4a4a4, 0x919191, 0x7e7e7e, 0x6b6b6b, 0x585858, 0x454545, 0x323232)
                                                .map(Color::new)
                                                .toArray(Color[]::new);
