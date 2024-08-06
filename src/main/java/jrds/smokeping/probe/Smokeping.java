af96142b2553ecb510c10b43c9e6d825866d96e5
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2024-08-06 15:02:08 +0200

    Adding handling of fping and using new ExternalCmdProbe API.

--- src/main/java/jrds/smokeping/probe/Smokeping.java
+++ src/main/java/jrds/smokeping/probe/Smokeping.java
@@ -5,7 +5,9 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import org.slf4j.event.Level;
@@ -19,34 +21,39 @@ import lombok.Setter;
 @ProbeBean({"node"})
 public class Smokeping extends ExternalCmdProbe implements IndexedProbe {
 
-    private static final Pattern colon = Pattern.compile(":");
-
     @Getter @Setter
     private String node = null;
+
+    private Pattern separator;
+    private Function<String, double[]> parse;
 
     @Override
     public Boolean configure() {
         if (node == null) {
             node = getHost().getDnsName();
         }
-        try {
-            InetAddress addr = InetAddress.getByName(node);
-            cmd = cmd +  " " + addr.getHostAddress();
-            return true;
-        } catch (UnknownHostException e) {
-            log(Level.ERROR, "host name %s unknown", node);
-            return false;
+        if (! cmd.endsWith("fping")) {
+            separator = Pattern.compile(":");
+            parse = this::parseSmallping;
+            try {
+                InetAddress addr = InetAddress.getByName(node);
+                setArgsList(List.of(addr.getHostAddress()));
+                return super.configure();
+            } catch (UnknownHostException e) {
+                log(Level.ERROR, "host name %s unknown", node);
+                return false;
+            }
+        } else {
+            separator = Pattern.compile(" ");
+            parse = this::parseFping;
+            setArgsList(List.of(/*"-b", "0",*/ "-C", "20", "-i", "0", "-p", "5", "-r", "0", "-N", "-q", node));
+            return super.configure();
         }
     }
 
     @Override
-    public Map<String, Number> getNewSampleValues() {
-        String smallping = launchCmd();
-        double[] values = colon.splitAsStream(smallping)
-                               .filter(s -> ! "U".equals(s))
-                               .mapToDouble(this::convertString)
-                               .filter(d -> ! Double.isNaN(d))
-                               .toArray();
+    protected Map<String, Number> resolveSampleValues(String smallping) {
+        double[] values = parse.apply(smallping);
         if (values.length > 20 || values.length == 0) {
             log(Level.ERROR, "smallping run failed: %s", smallping);
             return Collections.emptyMap();
@@ -60,6 +67,22 @@ public class Smokeping extends ExternalCmdProbe implements IndexedProbe {
         valuesMap.put("median", median(values));
         valuesMap.put("max", values[values.length -1]);
         return valuesMap;
+    }
+
+    private double[] parseSmallping(String result) {
+        return separator.splitAsStream(result)
+                        .filter(s -> ! "U".equals(s) && ! "N".equals(s))
+                        .mapToDouble(this::convertString)
+                        .filter(d -> ! Double.isNaN(d))
+                        .toArray();
+    }
+
+    private double[] parseFping(String result) {
+        return separator.splitAsStream(result.substring(result.indexOf(':') + 2))
+                        .filter(s -> ! "-".equals(s))
+                        .mapToDouble(s -> convertString(s) / 1000)
+                        .filter(d -> ! Double.isNaN(d))
+                        .toArray();
     }
 
     private double convertString(String value) {
